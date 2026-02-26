f2556de682e0b85fc0d3118f577ebeb91f0cb846
sailtyip <saltycroser@gmail.com>
2026-02-27 01:05:16 +0530

    added initial db

--- routes/router.js
+++ routes/router.js
@@ -2,18 +2,16 @@ import express from 'express';
 import pool from '../db.js';
 const router = express.Router();
 
-let links = [
+/*let links = [
     {org: "original", short: "org" }
-];
+]; */
 
-let count = 0;
-
-const shortner_logic = () =>{
+const shortner_logic = (id) =>{
     //const back_part_ind = org_link.lastIndexOf('/');
     //const back_part = org_link.slice(back_part_ind);
     //const front_part = org_link.slice(0,back_part_ind) + '/' ;
 
-    
+    /*
     let chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_abcdefghijklmnopqrstuvwxyz-";
     let string = "";
     let num = count;
@@ -27,50 +25,98 @@ const shortner_logic = () =>{
         }
     }
     count++;
-    return string;
+    return string;*/
+
+    const char = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let string ="";
+    while(id > 0){  
+        string = char[id%62] + string;
+        id = Math.floor(id/62);
+    }
+    return string || "0";
+
 };
 
 
 // @desc TO CREATE LINK OR GET LINK 
 // @route POST
-router.post('/',(req,res,next) =>{
-    if(!req.body || !req.body.org){
-        const err = Error("title or req body not properly sent");
-        err.status = 404;
-        return next(err);
+router.post('/',async(req,res,next) =>{
+    try{
+        if(!req.body || !req.body.org){
+            const err = new Error("title or req body not properly sent");
+            err.status = 400; //400 is for bad request
+            return next(err);
+        }
+        /*const exist = links.find((link) => link.org == req.body.org);  //exist is basically the object we get 
+        if(exist){
+            const err = new Error("the following link already exists");
+            err.status = 404;
+            return next(err);   
+        }
+
+        const exist_query = 'SELECT original_url FROM urls WHERE original_url = $1';
+        const exist_val = req.body.org;
+        const exists = await pool.query(exist_query,[exist_val]);*/
+
+        /*if(exists.rows.length){
+            const err = new Error("the following link already exists");
+            err.status = 409; //409 is for conflict 
+            return next(err);
+        }*/
+        const id_query = 'INSERT INTO urls (original_url) VALUES ($1) RETURNING id;';
+        const result_logic = await pool.query(id_query,[req.body.org]); 
+        const id = result_logic.rows[0].id;
+        const newShort = shortner_logic(result_logic.rows[0].id);
+        //const liob = {org: req.body.org, short:newShort};
+        //links.push(liob);
+        const insert_query = 'UPDATE urls SET short_code = $1 WHERE id = $2 RETURNING *;';
+        const result_post = await pool.query(insert_query,[newShort,id]);
+        res.status(201).json(result_post.rows[0]);
+        }
+    catch(err){
+        if(err.code === '23505'){
+            return res.status(409).json({msg:"already existss"});
+        }
+        next(err);
     }
-    const exist = links.find((link) => link.org == req.body.org);  //exist is basically the object we get 
-    if(exist){
-        const err = Error("the following link already exists");
-        err.status = 404;
-        return next(err);   
-    }
-    const newShort = shortner_logic();
-    const liob = {org: req.body.org, short:newShort};
-    links.push(liob);
-    res.status(201).json(liob);
-    }   
+}
 );
 
 //@desc TO SHOW THE LINK
 //@route GET 
-router.get('/',(req,res)=>{
-    res.status(200).json(links);
-
+router.get('/',async(req,res,next)=>{
+    //res.status(200).json(links);
+    try{
+    const result_get = await pool.query('SELECT * FROM urls;');
+    res.status(200).json(result_get.rows);
+    }
+    catch(err){
+        next(err);
+    }
 });
 
 //@desc REDIRECTS TO REAL LINK
 //@route GET
-router.get('/:code',(req,res,next)=>{
-    const code = req.params.code;
-    const real = links.find((link) => link.short == code);
+router.get('/:code',async (req,res,next)=>{
+    try{
+    /*const real = links.find((link) => link.short == code);
     if(!real){
         const err = Error("link not found");
         err.status = 404;
         return next(err);
+    }*/
+    const code_query = 'SELECT original_url FROM urls WHERE short_code = $1';
+    const result_code = await pool.query(code_query,[req.params.code]);
+    if(!result_code.rows.length){
+        const err = new Error("the following shor code couldnt be found")
+        err.status = 404;
+        return next(err);
     }
-    res.redirect(real.org); //redirects send 302 status by default
-
+    res.redirect(result_code.rows[0].original_url); //redirects send 302 status by default
+    }
+    catch(err){
+        next(err);
+    }
 });
 
 
